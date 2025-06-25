f64ff42a776f9f3e46c7ad8a0c2a3341ee062f17
James Appleton <jappleton101@gmail.com>
2025-06-25 11:18:43 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const session = require('express-session');
+const pg = require('pg');
+const redis = require('redis');
+const connectRedis = require('connect-redis');
+require('dotenv').config();
+
+const db = new pg.Client(process.env.DATABASE_URL);
+const redisClient = redis.createClient({ url: process.env.REDIS_URL });
+
+const app = express();
+
+let redisStore = new connectRedis.RedisStore({
+  client: redisClient,
+  prefix: 'amplisaas:',
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      maxAge: 3600000,
+    },
+    store: redisStore,
+  })
+);
+
+db.connect();
+redisClient.connect();
+
+app.get('/', (req, res) => {
+  return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.use('/', (err, req, res, next) => {
+  console.log(err);
+  const defaultError = {
+    log: 'Express error handler caught unknown error',
+    status: 500,
+    message: { err: 'An error occurred.' },
+  };
+  const errorObject = Object.assign({}, defaultError, err);
+  return res.status(errorObject.status).json(errorObject.message);
+});
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000');
+});
