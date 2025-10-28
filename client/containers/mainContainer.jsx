31c021b81220402ef05d2e60a7e63587d031a990
James Appleton <jappleton101@gmail.com>
2025-10-28 13:51:09 -0400

    junky

--- client/containers/mainContainer.jsx
+++ client/containers/mainContainer.jsx
@@ -4,43 +4,71 @@ import ToDoCard from "../components/ToDoList.jsx";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { AmplitudeBrowser } from "@amplitude/analytics-browser";
+import * as engagement from "@amplitude/engagement-browser";
 
 window.amplitude = new AmplitudeBrowser();
-amplitude.init("511911c0366c51e10cc03a5264a3808b", {
+
+await amplitude.init("511911c0366c51e10cc03a5264a3808b", {
   defaultTracking: false,
   autocapture: {
     pageViews: true,
-    attribution: true,
-    formInteractions: true,
-    sessions: true,
-    fileDownloads: true,
-    elementInteractions: {
-      cssSelectorAllowlist: [
-        "a",
-        "button",
-        "input",
-        "select",
-        "textarea",
-        "label",
-        "video",
-        "audio",
-        '[contenteditable="true" i]',
-        "[data-amp-default-track]",
-        ".amp-default-track",
-      ],
-      // by default, the above selectors are in the allow list
-      // When you use 'cssSelectorAllowlist', to target specific elements, set 'actionClickAllowlist'
-      // to ensure Amplitude tracks interactions with non-standard clickable elements during page transitions or DOM updates
-      actionClickAllowlist: ["div", "span", "h1", "h2", "h3", "h4", "h5", "h6"], // default settings
-      // Accepts CSS selectors that define which elements on the page should be tracked when the page changes
-      // for example, when a new visual element appears, or the click takes the user to a new page
-    },
-    networkTracking: {
-      captureRules: [{ statusCodeRange: "200-599" }],
-      // tracks Fetch/XHR requests capture rules can be specified to broaden or narrow default rules
-    },
+    attribution: false,
+    formInteractions: false,
+    sessions: false,
+    fileDownloads: false,
+    // elementInteractions: {
+    //   cssSelectorAllowlist: [
+    //     "a",
+    //     "button",
+    //     "input",
+    //     "select",
+    //     "textarea",
+    //     "label",
+    //     "video",
+    //     "audio",
+    //     '[contenteditable="true" i]',
+    //     "[data-amp-default-track]",
+    //     ".amp-default-track",
+    //   ],
+    //   // by default, the above selectors are in the allow list
+    //   // When you use 'cssSelectorAllowlist', to target specific elements, set 'actionClickAllowlist'
+    //   // to ensure Amplitude tracks interactions with non-standard clickable elements during page transitions or DOM updates
+    //   actionClickAllowlist: ["div", "span", "h1", "h2", "h3", "h4", "h5", "h6"], // default settings
+    //   // Accepts CSS selectors that define which elements on the page should be tracked when the page changes
+    //   // for example, when a new visual element appears, or the click takes the user to a new page
+    // },
+    // networkTracking: {
+    //   captureRules: [{ statusCodeRange: "200-599" }],
+    //   // tracks Fetch/XHR requests capture rules can be specified to broaden or narrow default rules
+    // },
   },
 });
+
+engagement.init("511911c0366c51e10cc03a5264a3808b");
+await window.engagement.boot({
+  user: {
+    // User Provider: Guides and Surveys requires either user_id or device_id for user identification
+    user_id: "TEST",
+  },
+  integrations: [
+    {
+      track: (event) => {
+        amplitude.track(event.event_type, event.event_properties);
+        // analytics.track(event.event_type, event.event_properties);
+      },
+    },
+  ],
+});
+
+console.log(amplitude);
+console.log(engagement);
+// console.log(amplitude.getInstance());
+
+// const deviceId = amplitude.getInstance().getDeviceId();
+// const userId = amplitude.getInstance().options.userId; // or use your own user ID variable if set
+
+// console.log(userId);
+// console.log(deviceId);
 
 function MainContainer() {
   const [view, setView] = useState("");
