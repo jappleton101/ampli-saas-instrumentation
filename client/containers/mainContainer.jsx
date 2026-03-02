7c92b8142ebe6a49a535eb3f17ce5ce8944746f9
James Appleton <jappleton101@gmail.com>
2026-03-02 12:18:11 -0500

    initial commit

--- client/containers/mainContainer.jsx
+++ client/containers/mainContainer.jsx
@@ -1,7 +1,4 @@
 import * as React from "react";
-import ResponsiveAppBar from "../components/NavBar.jsx";
-import ToDoCard from "../components/ToDoList.jsx";
-import { useState, useEffect } from "react";
 import axios from "axios";
 import { AmplitudeBrowser } from "@amplitude/analytics-browser";
 import { plugin as engagementPlugin } from "@amplitude/engagement-browser";
@@ -10,15 +7,14 @@ import { Experiment } from "@amplitude/experiment-js-client";
 window.amplitude = new AmplitudeBrowser();
 amplitude.add(engagementPlugin());
 
-// your Amplitude API Key
 amplitude.init("511911c0366c51e10cc03a5264a3808b", {
   defaultTracking: false,
   autocapture: {
     pageViews: true,
-    attribution: false,
-    formInteractions: false,
-    sessions: false,
-    fileDownloads: false,
+    attribution: true,
+    formInteractions: true,
+    sessions: true,
+    fileDownloads: true,
     elementInteractions: true,
     networkTracking: {
       captureRules: [{ statusCodeRange: "200-599" }],
@@ -28,73 +24,20 @@ amplitude.init("511911c0366c51e10cc03a5264a3808b", {
 
 let variant;
 
-// This must be a deployment key that is in your amplitude instance and associated with the project of the API key used above
 const experiment = Experiment.initializeWithAmplitudeAnalytics(
   "client-TG1laGEVQ6ESxIBi0NHo9emqhMmufueg",
 );
 
-await experiment.fetch(); // retrieves flags and variants for a user
+await experiment.fetch();
 
-// This must be a flag key associated with an experiment in your Amplitude Instance
-// the actual feature display logic is in the ToDoList Component
-variant = experiment.variant("experimental-button"); // retrieves variant, fires exposure event
+variant = experiment.variant("experimental-button");
 
-variant.value; // returns the flag value for the feature flag/experiment
+variant.value;
 
 function MainContainer() {
-  const [view, setView] = useState("");
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  async function handleSessionAction(action) {
-    if (action === "logout") {
-      const response = await axios.post("/users/logout");
-      setIsLoggedIn(false);
-      setView("");
-    }
-
-    if (action === "login") {
-      setIsLoggedIn(true);
-    }
-  }
-
-  const setViewHandler = (e) => {
-    setView(e.target.id);
-  };
-
-  async function getSession() {
-    try {
-      const response = await axios.get("/users/session");
-      return response;
-    } catch (error) {
-      console.error(error);
-      alert("Error validating user session. Please log in again.");
-    }
-  }
-
-  async function getUserTasks() {}
-
-  useEffect(() => {
-    getSession().then((data) => {
-      if (data && data.data.sessionData) {
-        setIsLoggedIn(true);
-      }
-    });
-  }, [isLoggedIn]);
-
   return (
     <>
-      <ResponsiveAppBar
-        setViewHandler={setViewHandler}
-        isLoggedIn={isLoggedIn}
-        id="responsive-navbar"
-        currentView={view}
-        handleSessionAction={handleSessionAction}
-        amplitude={amplitude}
-      />
-      <div id="main-container">
-        <br />
-        <ToDoCard variantValue={variant ? variant.value : null} />
-      </div>
+      <h1>PLACEHOLDER</h1>
     </>
   );
 }
