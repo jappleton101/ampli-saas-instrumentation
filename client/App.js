7abe1dcdf25eb407690d885ed43fe079eafa96e7
James Appleton <jappleton101@gmail.com>
2026-03-05 16:04:12 -0500

    post-demo

--- client/App.js
+++ client/App.js
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import MainContainer from './containers/mainContainer.jsx';
 import { AmplitudeBrowser } from "@amplitude/analytics-browser";
-import { plugin as engagementPlugin } from "@amplitude/engagement-browser";
+// import { plugin as engagementPlugin } from "@amplitude/engagement-browser";
 import { Experiment } from "@amplitude/experiment-js-client";
 
 window.amplitude = new AmplitudeBrowser();
-amplitude.add(engagementPlugin());
+// amplitude.add(engagementPlugin());
 
 amplitude.init("99698bc4876a943672eae07e721f4c2d", {
   defaultTracking: false,
@@ -32,6 +32,8 @@ const experiment = Experiment.initializeWithAmplitudeAnalytics(
 await experiment.fetch();
 
 variant = experiment.variant("testexperiment");
+
+console.log("This user is assigned to ", variant.value);
 
 const queryClient = new QueryClient();
 
